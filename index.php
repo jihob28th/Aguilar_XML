2ad9563d49c98658a06372888431dcb243d2dd1f
jihob28th <60122546+jihob28th@users.noreply.github.com>
2020-01-27 22:43:00 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+    $rss = '<?xml version="1.0" encoding="UTF-8"?>';
+    $rss .= '<rss version="2.0">';
+    $rss .= '<channel>';
+
+    $connect = mysqli_connect("dbrojasdev.cjw42bnplsor.us-east-1.rds.amazonaws.com", "admin", "root1234", "db_1820841") or die (mysqli_error($connect));
+    $sql = "SELECT * FROM tbl_shoes ORDER BY shoe_ID ASC;";
+    $query = mysqli_query($connect,$sql) or die (mysqli_error($connect));
+    while($records= mysqli_fetch_assoc($query)) {
+        extract ($records);
+
+        $rss .= '<shoes>';
+        $shoe_Name1 .= '<name>, &nbsp;&nbsp;' . $shoe_Name . '</name>';
+        $shoe_Brand1 .= '<brand>, &nbsp;&nbsp;' . $shoe_Brand . '</brand>';
+        $shoe_Type1 .= '<type>, &nbsp;&nbsp;' . $shoe_Type . '</type>';
+        $shoe_Cost1 .= '<cost>, &nbsp;&nbsp;' . $shoe_Cost . '</cost>';
+        $rss .= '</shoes>';
+    }
+
+    $rss .= '</channel>';
+    $rss .= '</rss>';
+
+    
+?>
+<html>
+    <head>
+        <title> </title>
+    </head>
+    <body>
+        <ul>
+            <li><?php echo $shoe_Name1 ?> </li>
+            <li><?php echo $shoe_Brand1 ?> </li>
+            <li><?php echo $shoe_Type1 ?> </li>
+            <li><?php echo $shoe_Cost1 ?> </li>
+        </ul>
+    </body>
+</html>
