5663afeecb6647a5b118ed72b781144abf054bcf
jihob28th <60122546+jihob28th@users.noreply.github.com>
2020-01-28 11:17:28 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+ $domOBJ = new DOMDocument();
+ $domOBJ->load("https://xml-aguilar.herokuapp.com/");//XML page URL
+ 
+ $content = $domOBJ->getElementsByTagName("shoes");
+ 
+ ?>
+ <ul>
+    <?php
+ foreach( $content as $data )
+ {
+   $name = $data->getElementsByTagName("name ")->item(0)->nodeValue;
+   $brand = $data->getElementsByTagName("brand")->item(0)->nodeValue;
+   $type = $data->getElementsByTagName("type")->item(0)->nodeValue;
+   $cost= $data->getElementsByTagName("cost")->item(0)->nodeValue;
+   
+  
+   echo "<li>$name 
+            <ul>
+                <li>$brand</li>
+				<li>$type</li>
+				<li>$cost</li>
+            </ul>
+        </li>";
+ }
+?>
+</ul>
+
