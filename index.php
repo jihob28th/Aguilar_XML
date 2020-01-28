dc214efbe83fd631a3899c9eecf0d17a242611ad
jihob28th <60122546+jihob28th@users.noreply.github.com>
2020-01-28 11:28:34 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
  $domOBJ = new DOMDocument();
- $domOBJ->load("https://xml-aguilar.herokuapp.com/rss.php");//XML page URL
+ $domOBJ->load("https://xmlexercise1-aguilar.herokuapp.com/rss.php");//XML page URL
  
  $content = $domOBJ->getElementsByTagName("shoes");
  
@@ -9,7 +9,7 @@
     <?php
  foreach( $content as $data )
  {
-   $name = $data->getElementsByTagName("name ")->item(0)->nodeValue;
+   $name = $data->getElementsByTagName("name")->item(0)->nodeValue;
    $brand = $data->getElementsByTagName("brand")->item(0)->nodeValue;
    $type = $data->getElementsByTagName("type")->item(0)->nodeValue;
    $cost= $data->getElementsByTagName("cost")->item(0)->nodeValue;
@@ -18,8 +18,8 @@
    echo "<li>$name 
             <ul>
                 <li>$brand</li>
-				<li>$type</li>
-				<li>$cost</li>
+		<li>$type</li>
+		<li>$cost</li>
             </ul>
         </li>";
  }
